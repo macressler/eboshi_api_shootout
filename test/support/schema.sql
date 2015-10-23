ba57b0130340250e3f8c6e0814336db8afa482b0
Eric Hanson <eric@aquameta.com>
2015-10-22 19:14:25 -0700

    Use simpler/more standarder SQL in schema def
    Not sure if switching datetime (non-standard) to timestamp will cause problems.
    Changed the created_at and updated_at default to NOW().
    Changed varchars to text.
    Removed MyISAM engine selection stuff.
    
    works in both postgresql and mysql:
    
    ericbook:support eric$ cat schema.sql | psql eboshi
    DROP TABLE
    CREATE TABLE
    DROP TABLE
    CREATE TABLE
    ericbook:support eric$ cat schema.sql | mysql -uroot eboshi
    ericbook:support eric$

--- test/support/schema.sql
+++ test/support/schema.sql
@@ -1,29 +1,29 @@
-DROP TABLE IF EXISTS `clients`;
-CREATE TABLE `clients` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `address` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `city` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `state` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `zip` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `country` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `email` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `contact` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `phone` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `created_at` datetime DEFAULT NULL,
-  `updated_at` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+DROP TABLE IF EXISTS clients;
+CREATE TABLE clients (
+  id serial,
+  name text,
+  address text,
+  city text,
+  state text,
+  zip text,
+  country text,
+  email varchar(255),
+  contact text,
+  phone text,
+  created_at timestamp DEFAULT NOW(),
+  updated_at timestamp DEFAULT NOW(),
+  PRIMARY KEY (id)
+);
 
-DROP TABLE IF EXISTS `users`;
-CREATE TABLE `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) DEFAULT NULL,
-  `email` varchar(255) DEFAULT NULL,
-  `crypted_password` varchar(255) DEFAULT NULL,
-  `password_salt` varchar(255) DEFAULT NULL,
-  `created_at` datetime DEFAULT NULL,
-  `updated_at` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE (`email`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+  id serial,
+  name text,
+  email varchar(255),
+  crypted_password text,
+  password_salt text,
+  created_at timestamp DEFAULT NOW(),
+  updated_at timestamp DEFAULT NOW(),
+  PRIMARY KEY (id),
+  UNIQUE (email)
+);
